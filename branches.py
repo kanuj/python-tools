59cf6d92cf4310aa4cd4be5c1ee7ac292cd26f08
Kanuj Bhatnagar <kanujb@brickred.com>
2012-10-11 18:01:44 +0530

    Python script to fast forward out of date branches, with libnotify support

---
+++ branches.py
@@ -0,0 +1,25 @@
+import subprocess
+import os
+
+subprocess.check_output("git fetch")
+
+try:
+	total_branches = subprocess.check_output("git remote show origin | grep 'local out of date'")
+except subprocess.CalledProcessError:
+	os.system('notify-send -u low "No new branches"')
+	exit(1)
+
+branches_list = total_branches.split("\n")
+
+i = 0
+
+for branch in branches_list:
+
+	each_branch = branch.split("    ")	
+	br = each_branch[1:2]
+	if len(br) != 0:
+		os.system("git checkout " + br.pop())
+		os.system("git pull")
+		i = i+1
+	
+os.system("notify-send -u low 'Number of branches: "+str(i)+"'")
